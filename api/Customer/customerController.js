4b96f67b24bad83aaa841cd79fc58676d13498a9
najimRizky <najim.rizky@student.umn.ac.id>
2023-03-05 16:57:41 +0700

    refactor: remove unused code

--- api/Customer/customerController.js
+++ api/Customer/customerController.js
@@ -2,44 +2,6 @@ const responseParser = require("../../helper/responseParser")
 const sendEmailConfirmation = require("../../server/sendEmailConfirmation")
 const Customer = require("./customerModel")
 
-
-const registerCustomer = async (req, res) => {
-    const { email, password, full_name } = req.body
-    try {
-        const customer = await Customer.register({ email, password, full_name })
-
-        sendEmailConfirmation({
-            email: customer.email,
-            fullName: customer.full_name,
-            confirmationToken: customer.confirmation_token
-        })
-
-        return responseParser({ 
-            data: {
-                email: customer.email,
-                full_name: customer.full_name
-            }, 
-            message: "New account created, check your email to confirm your account!" 
-        }, res)
-    } catch (err) {
-        return responseParser({ status: 400, error: err.message }, res)
-    }
-}
-
-const confirmCustomer = async (req, res) => {
-    const { token } = req.query
-
-    const customer = await Customer.findOne({confirmation_token: token})
-
-    if(customer) {
-        await Customer.updateOne({_id: customer._id}, {$set: {confirmed: true}})
-        return res.send("Your email has been confirmed. You may login now.")
-    } else {
-        return responseParser({status: 404, error: "Invalid confirmation link"}, res)
-    }
-}
-
 module.exports = {
-    registerCustomer,
-    confirmCustomer
+    
 }
--- api/Customer/customerRoutes.js
+++ api/Customer/customerRoutes.js
@@ -2,12 +2,10 @@ const express = require("express")
 const router = express.Router()
 const customerValidation = require("./customerValidation")
 
-const { registerCustomer, confirmCustomer } = require("./customerController")
+const customerController = require("./customerController")
 const checkValidation = require("../../helper/checkValicdation")
 
 /** @see /api/customer */
 
-router.post("/register", customerValidation.register, checkValidation, registerCustomer)
-router.get("/confirm", confirmCustomer)
 
 module.exports = router
