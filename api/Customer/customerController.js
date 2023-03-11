d687e12baa631b1ec05445881cb78087f20909c4
najimRizky <najim.rizky@student.umn.ac.id>
2023-03-11 20:33:05 +0700

    feat: replace upload image bucket

--- api/Customer/customerController.js
+++ api/Customer/customerController.js
@@ -5,15 +5,19 @@ import Customer from "./customerModel.js"
 
 const editProfile = async (req, res) => {
     try {
-        const customerId = req.user._id
+        const { _id } = req.user
+        const { full_name } = req.body
+        const {profile_image} = await Customer.findById(_id, ["profile_image"])
+
         if (req.file) {
-            const url = await uploadToBucket({ req })
-            await Customer.updateOne({ _id: customerId, full_name: req.body.full_name, profile_image: url })
+            const url = await uploadToBucket({ req, currentUrl: profile_image })
+            await Customer.updateOne({ _id, full_name, profile_image: url })
         } else {
-            await Customer.updateOne({ _id: customerId, full_name: req.body.full_name })
+            await Customer.updateOne({ _id, full_name })
         }
         return responseParser({ status: 200 }, res)
     } catch (err) {
+        console.log(err)
         return responseParser({ status: 404 }, res)
     }
 }
@@ -32,10 +36,10 @@ const updateBalance = async (req, res) => {
     try {
         const { amount } = req.body
         const { _id } = req.user
-        
+
         await Customer.updateOne({ _id }, { $inc: { balance: amount } })
         const updatedCustomer = await Customer.findById(_id)
-        
+
         return responseParser({ status: 200, data: { balance: updatedCustomer.balance } }, res)
     } catch (err) {
         return responseParser({ status: 404 }, res)
--- helper/uploadToBucket.js
+++ helper/uploadToBucket.js
@@ -7,16 +7,19 @@ import { nanoid } from "nanoid"
 const S3 = new AWS.S3(BucketConfig)
 const CLOUDFLARE_PUBLIC_BUCKET_URL = process.env.CLOUDFLARE_PUBLIC_BUCKET_URL
 
-const uploadToBucket = async ({ req }) => {
+const uploadToBucket = async ({ req, currentUrl }) => {
     const { path, mimetype, filename } = req.file
     const extension = mime.extension(mimetype)
 
-    const uniqueFileName = nanoid(5) + filename
+    const fileNameWithExt = currentUrl === "" ? "" : currentUrl.substring(currentUrl.lastIndexOf("/") + 1)
+    const currentFileName = currentUrl === "" ? "" : fileNameWithExt.substring(0, fileNameWithExt.lastIndexOf("."));
+
+    const uniqueFileName = currentFileName === "" ? nanoid(5) + filename : currentFileName;
 
     const params = {
         ...BucketParams,
         Body: fs.createReadStream(path),
-        Key: `${uniqueFileName}.${extension}`
+        Key: `${uniqueFileName}`
     }
 
     return new Promise((resolve, reject) => {
@@ -26,7 +29,7 @@ const uploadToBucket = async ({ req }) => {
             }
             if (data) {
                 fs.unlinkSync(path); // delete
-                const url = `${CLOUDFLARE_PUBLIC_BUCKET_URL}/${data.Key}`;
+                const url = `${CLOUDFLARE_PUBLIC_BUCKET_URL}/${data.Key}.${extension}`;
                 resolve(url)
             }
         });
