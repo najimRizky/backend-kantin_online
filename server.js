bf09c30a4c81f860a7b5d8168724d55d608a2a00
najimRizky <najim.rizky@student.umn.ac.id>
2023-03-11 18:12:11 +0700

    refactor: remove login & register entities

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ import serverListen from "./server/serverListen.js"
 import requestLogger from "./server/requestLogger.js" 
 
 /** @Routes */
-import registerRoutes from "./api/Register/registerRoutes.js"
-import loginRoutes from "./api/Login/loginRoutes.js"
 import uploadRoutes from "./api/Upload/uploadRoutes.js"
 import customerRoutes from "./api/Customer/customerRoutes.js"
 import accountRoutes from "./api/Account/accountRoutes.js"
@@ -26,8 +24,6 @@ requestLogger(server)
 server.get('/', (_, res) => {
     return responseParser({ data: "REST API Kantin UMN", status: 200 }, res)
 })
-server.use("/register", registerRoutes)
-server.use("/login", loginRoutes)
 server.use("/upload", uploadRoutes)
 server.use("/customer", customerRoutes)
 server.use("/account", accountRoutes)
