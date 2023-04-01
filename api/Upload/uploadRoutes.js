0f85527501b2bb9dda1ca0df3342c399d2bf7d7d
najimRizky <najim.rizky@student.umn.ac.id>
2023-04-01 15:30:06 +0700

    feat: migrate aws-sdk v2 to v3

--- api/Upload/uploadRoutes.js
+++ api/Upload/uploadRoutes.js
@@ -1,34 +1,46 @@
 import express from "express"
 import multer from "multer"
 import fs from "fs"
-import AWS from "aws-sdk"
 import { BucketConfig, BucketParams } from "./../../config/bucketConfig.js"
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
+import errorHandler from "../../helper/errorHandler.js"
 
 const upload = multer({ dest: "uploads/" })
 const router = express.Router()
-const S3 = new AWS.S3(BucketConfig)
+
+const S3 = new S3Client({
+    region: "auto",
+    endpoint: BucketConfig.endpoint,
+    credentials: {
+        accessKeyId: BucketConfig.accessKeyId,
+        secretAccessKey: BucketConfig.secretAccessKey
+    }
+})
 const CLOUDFLARE_PUBLIC_BUCKET_URL = process.env.CLOUDFLARE_PUBLIC_BUCKET_URL
 
 /** @see /upload */
 
 router.post('/', upload.single("images"), async (req, res) => {
     const path = req.file.path
-    const params = {
+    const Body = fs.createReadStream(path)
+    const Key = req.file.originalname
+    
+    const params = new PutObjectCommand({
         ...BucketParams,
-        Body: fs.createReadStream(path),
-        Key: req.file.originalname
-    }
+        Body,
+        Key
+    })
 
-    S3.upload(params, (err, data) => {
-        if (err) {
-            return res.status(500).send(err)
-        }
-        if (data) {
-            fs.unlinkSync(path); // delete
-            const url = `${CLOUDFLARE_PUBLIC_BUCKET_URL}/${data.Key}`;
-            return res.send({ url, data })
-        }
-    });
+    try {
+        const data = await S3.send(params)
+        fs.unlinkSync(path); // delete
+        const url = `${CLOUDFLARE_PUBLIC_BUCKET_URL}/${Key}`;
+        
+        return res.send({ url, data })
+    } catch (err) {
+        fs.unlinkSync(path); // delete
+        return errorHandler(err, res)
+    }
 });
 
 export default router
