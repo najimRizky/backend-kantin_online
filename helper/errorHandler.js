22014478ea4fe649775fe4b80d2a2992f536ba3e
najimRizky <najim.rizky@student.umn.ac.id>
2023-04-02 14:45:03 +0700

    feat: change errorHandler

--- helper/errorHandler.js
+++ helper/errorHandler.js
@@ -1,9 +1,13 @@
 import responseParser from "./responseParser.js"
 
 const errorHandler = (err, res) => {
+    const error = err.split(" || ")
+    const message = error[0] || "An unexpected error has occurred"
+    const status = error[1] || 500
+
     return responseParser({ 
-        status: err.status || 500, 
-        error: err.message || "Internal Server Error" 
+        status: status, 
+        error: message 
     }, res)
 }
 
