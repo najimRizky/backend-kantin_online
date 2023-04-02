fd90cf60a356718e3307f29b91a1d69ab8d9956b
najimRizky <najim.rizky@student.umn.ac.id>
2023-04-02 15:36:12 +0700

    feat: update errorHandler

--- helper/errorHandler.js
+++ helper/errorHandler.js
@@ -1,9 +1,12 @@
 import responseParser from "./responseParser.js"
 
 const errorHandler = (err, res) => {
-    const error = err.split(" || ")
-    const message = error[0] || "An unexpected error has occurred"
-    const status = error[1] || 500
+    const customError = err.message.includes("||") 
+
+    const error = customError ? err.message.split("||") : err.message
+    
+    const status = customError ? Number(error[1] || 500) : 500
+    const message = customError ? (error[0] || undefined) : error
 
     return responseParser({ 
         status: status, 
