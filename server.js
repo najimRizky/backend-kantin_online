444d235c3e8c93d63a3f1c83ee739e5fca9457ee
najimRizky <najim.rizky@student.umn.ac.id>
2023-04-13 09:43:54 +0700

    feat: add undefined route handler

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 console.clear()
 import "./config/loadEnv.js"
 
-/** @Library */ 
+/** @Library */
 import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
 
-/** @Function */ 
+/** @Function */
 import responseParser from "./helper/responseParser.js"
 import connectDatabase from "./database/connectDatabase.js"
 import serverListen from "./server/serverListen.js"
-import requestLogger from "./server/requestLogger.js" 
+import requestLogger from "./server/requestLogger.js"
 
 /** @Routes */
 import uploadRoutes from "./api/Upload/uploadRoutes.js"
@@ -22,7 +22,7 @@ import otherRoutes from "./api/Other/otherRoutes.js"
 import cartRoutes from "./api/Cart/cartRoutes.js"
 import orderRoutes from "./api/Order/orderRoutes.js"
 
-/** @Initialization */ 
+/** @Initialization */
 const server = express()
 server.use(express.json())
 server.set('view engine', 'ejs');
@@ -40,6 +40,16 @@ server.use("/tenant", tenantRoutes)
 server.use("/menu", menuRoutes)
 server.use("/cart", cartRoutes)
 server.use("/order", orderRoutes)
+
+
+server.all("*", (req, res) => {
+    const message = `The requested endpoint could not be found on the server.`
+    const requestDetail = {
+        method: req.method,
+        endpoint: req.path
+    }
+    return responseParser({ status: 404, error: message, data: { request_detail: requestDetail } }, res)
+})
 
 const ENABLE_DB = eval(process.env.ENABLE_DB)
 if (ENABLE_DB) {
