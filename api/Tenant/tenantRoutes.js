a63e97d248ca517869695b5ee507d076617fdd50
najim <najim.rizky@jkt1.ebdesk.com>
2023-04-17 11:37:38 +0700

    docs: add documentation on tenantroutes

--- api/Tenant/tenantRoutes.js
+++ api/Tenant/tenantRoutes.js
@@ -9,6 +9,7 @@ const router = express.Router()
 
 /** @see /tenant */
 
+/** @REGISTER_TENANT */
 router.post(
     "/register",
     tenantValidation.register,
@@ -16,11 +17,13 @@ router.post(
     tenantController.register
 )
 
+/** @GET_ALL_TENANT */
 router.get(
     "/",
     tenantController.getAll
 )
 
+/** @GET_DETAIL_TENANT */
 router.get(
     "/:_id",
     tenantValidation.getDetail,
@@ -28,12 +31,14 @@ router.get(
     tenantController.getDetail
 )
 
+/** @GET_PROFILE_TENANT */
 router.get(
     "/profile",
     requireAuth(roleConfig.tenant),
     tenantController.getProfile
 )
 
+/** @UPDATE_PROFILE_TENANT */
 router.put(
     "/profile",
     requireAuth(roleConfig.tenant),
