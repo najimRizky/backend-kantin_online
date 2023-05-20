047e2f6d4f0ea89d237b57048cb5740d762585df
najimRizky <najim.rizky@student.umn.ac.id>
2023-05-20 17:33:40 +0700

    feat: add severity to socketService

--- server/socketService.js
+++ server/socketService.js
@@ -6,14 +6,18 @@ export default class SocketService {
     this.io = new Server(server);
     this.io.on('connection', (socket) => {
       socket.on('tenant:join', (data) => {
-        socket.join(data);
+        console.log('tenant:join', data);
+      })
+      socket.on('customer:join', (data) => {
+        console.log('customer:join', data);
       })
     });
   }
 
-  emiter({event, url, message}) {
+  emiter({event, url, message, severity}) {
     if (event && url && message) {
-      this.io.emit(event, {url , message});
+      console.log('emiter', event, url, message);
+      this.io.emit(event, {url , message, severity});
     }
   }
 }
